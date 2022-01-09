661eb1594730c12cca51379ead2b8f8fde094bae
Madison M <mcnicol.madison@gmail.com>
2022-01-08 19:46:32 -0700

    Adding PyPoll.py file

---
+++ Python_practice.py
@@ -0,0 +1,135 @@
+print("Hello World")
+ballots = 1,341
+print(5 + 2 * 3) 
+print(8 // 5 - 3)
+print(8 + 22 * 2 - 4)
+print(16 - 3 / 2 + 7 - 1)
+print(3 ** 3 % 5)
+print(5 + 9 * 3 / 2 - 4)
+print((5 + 2) * 3)
+print((8 // 5) - 3)
+print(8 + (22 * (2 - 4)))
+print(16 - 3 / (2 + 7) - 1)
+print(3 ** (3 % 5))
+print(5 + (9 * 3 / 2 - 4)) and (5 + (9 * 3 / (2 - 4)))
+counties = ["Arapahoe", "Denver","Jefferson"]
+print(counties)
+print(counties[2])
+print(counties[-1])
+print(len(counties))
+print(counties[0:2])
+counties.append("El Paso")
+print(counties)
+counties.insert(2, "El Paso")
+print(counties)
+counties.remove("El Paso")
+print(counties)
+counties.pop(3)
+print(counties)
+counties[2] = "El Paso"
+print(counties)
+counties_tuple = ("Arapahoe", "Denver", "Jefferson")
+print(len(counties_tuple))
+print(counties_tuple[1])
+counties_dict = {}
+counties_dict["Arapahoe"] = 422829
+print(counties_dict)
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+print(counties_dict)
+print(len(counties_dict))
+print(counties_dict.items())
+print(counties_dict.keys())
+print(counties_dict.values())
+print(counties_dict.get("Denver"))
+voting_data = []
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+print(voting_data)
+temperature = int(input("What is the temperature outside? "))
+
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+#What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
+# What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.")
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso is in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+for county in counties:
+    print(county)
+numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
+for num in range(5):
+    print(num)
+for i in range(len(counties)):
+    print(counties[i])
+for county in counties_dict:
+    print(county)
+for county in counties_dict.keys():
+    print(county)
+for voters in counties_dict.values():
+    print(voters)
+for county in counties_dict:
+    print(counties_dict[county])
+for county in counties_dict:
+    print(counties_dict.get(county))
+for county, voters in counties_dict.items():
+    print(county, voters)
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+for county_dict in voting_data:
+    print(county_dict['county'])
